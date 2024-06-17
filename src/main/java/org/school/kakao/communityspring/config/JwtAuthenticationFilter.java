919a0ab6a922c13353114665b1c2d99d383f19bf
ZZ3n <ndsrhkd@naver.com>
2024-06-17 20:59:08 +0900

    ADD, JWT filter request matcher

--- src/main/java/org/school/kakao/communityspring/config/JwtAuthenticationFilter.java
+++ src/main/java/org/school/kakao/communityspring/config/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,22 +18,27 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
-@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final RequestMatcher requestMatcher;
     private final AuthenticationFailureHandler failureHandler = new AuthenticationEntryPointFailureHandler(
             new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        if (!requestMatcher.matches(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         String authorizationHeader = request.getHeader(JwtConstants.AUTHORIZATION_HEADER);
         if (authorizationHeader == null) {
-            logger.debug("Authorization header is empty");
+            log.debug("Authorization header is empty, from : {}", request.getRequestURI());
             filterChain.doFilter(request, response);
             return;
         }
@@ -46,7 +52,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             filterChain.doFilter(request, response);
         } catch (AuthenticationException exception) {
-            logger.debug("AUTHENTICATION FAILED", exception);
+            log.debug("AUTHENTICATION FAILED", exception);
             failureHandler.onAuthenticationFailure(request, response, exception);
         }
     }
---
+++ src/main/java/org/school/kakao/communityspring/config/JwtConfiguration.java
@@ -0,0 +1,24 @@
+package org.school.kakao.communityspring.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class JwtConfiguration {
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(new OrRequestMatcher(
+                List.of(
+                        AntPathRequestMatcher.antMatcher("/posts/**"),
+                        AntPathRequestMatcher.antMatcher("/users/**"),
+                        AntPathRequestMatcher.antMatcher("/comments/**")
+                )
+        ));
+    }
+}
