758f8babdff8d728c78a4bb5548d08f46fea4f68
ZZ3n <ndsrhkd@naver.com>
2024-07-03 22:19:26 +0900

    test final

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,13 +14,15 @@ DB_URL=$(aws ssm get-parameter --name /week-eleven/DB_URL --query "Parameter.Val
 DB_USER=$(aws ssm get-parameter --name /week-eleven/DB_USER --query "Parameter.Value")
 DB_PASSWORD=$(aws ssm get-parameter --name /week-eleven/DB_PASSWORD --query "Parameter.Value")
 
-echo "$DB_URL $DB_PASSWORD $DB_DB_USER" >> $START_LOG
+echo "[$NOW] DB 정보 : $DB_URL $DB_PASSWORD $DB_USER" >> $START_LOG
 
 echo "[$NOW] $SERVER_JAR 복사" >> $START_LOG
 cp $UPLOADED_JAR $SERVER_JAR
 
 echo "[$NOW] > $SERVER_JAR 실행" >> $START_LOG
-nohup java -jar -Dspring.profiles.active=dev $SERVER_JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar -Dspring.profiles.active=dev \
+  -DDB_URL=$DB_URL -DDB_PASSWORD=$DB_PASSWORD -DDB_USER=$DB_USER \
+  $SERVER_JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $SERVER_JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
